1188fdd4a8dadbde074d74abe6e732d662e68ccb
r0bbar <r0bbarh00d@gmail.com>
2025-02-07 06:42:14 +0800

    any_exchange initial commit

---
+++ siglab_py/exchanges/any_exchange.py
@@ -0,0 +1,18 @@
+from ccxt.base.exchange import Exchange
+
+'''
+The idea here is, if we were to trade thru for example IKBR, which is a tradfi broker not supported by CCXT, we would implement IBKR like CCXT implement for crypto exchanges.
+Concrete implementation will:
+a. Inherit from AnyExchange, thus ccxt Exchange class
+b. Override ccxt basic functions
+    - load_markets
+    - create_order
+    - update_order
+    - cancel_order
+    - order amount rounding: amount_to_precision
+    - order price rounding: price_to_precision
+    ... etc
+'''
+class AnyExchange(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
