c41985682dc67df9fb0b0ee8c631acc274e958f3
r0bbar <r0bbarh00d@gmail.com>
2025-03-24 07:38:01 +0800

    typo, misc

--- siglab_py/ordergateway/gateway.py
+++ siglab_py/ordergateway/gateway.py
@@ -461,7 +461,7 @@ async def execute_one_position(
             dt_now : datetime = datetime.now()
 
             slice_amount_in_base_ccy : float = slice.amount
-            rounded_slice_amount_in_base_ccy = slice_amount_in_base_ccy / multiplier # After devided by multiplier, rounded_slice_amount_in_base_ccy in number of contracts actually (Not in base ccy).
+            rounded_slice_amount_in_base_ccy = slice_amount_in_base_ccy / multiplier # After divided by multiplier, rounded_slice_amount_in_base_ccy in number of contracts actually (Not in base ccy).
             rounded_slice_amount_in_base_ccy = exchange.amount_to_precision(position.ticker, rounded_slice_amount_in_base_ccy) # type: ignore
             rounded_slice_amount_in_base_ccy = float(rounded_slice_amount_in_base_ccy) if rounded_slice_amount_in_base_ccy else 0
             rounded_slice_amount_in_base_ccy = rounded_slice_amount_in_base_ccy if rounded_slice_amount_in_base_ccy>min_amount else min_amount
