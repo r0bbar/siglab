01156045cf4a750b2e9805cb1a6289f0922da2cb
r0bbar <r0bbarh00d@gmail.com>
2025-12-19 11:24:51 +0800

    deribit.py: fetch_position override CCXT implementation to swap 'notional' with 'contracts'

--- siglab_py/exchanges/deribit.py
+++ siglab_py/exchanges/deribit.py
@@ -3,8 +3,6 @@ from typing import Dict, Any
 import ccxt
 import ccxt.pro as ccxtpro
 
-from exchanges.any_exchange import AnyExchange
-
 '''
 Why override load_markets?
     Deribit is one of the OG CEX. If you look at 'contractSize' for BTC/USDC:USDC, a linear perp, for example:
@@ -14,6 +12,35 @@ Why override load_markets?
     The general prevailing convention in CCXT is: 'amount' should be quoted in '# contracts'.
     Why CCXT not fix Deribit, so that it follows the prevailing convention? This is because this would be a breaking changes.
     Thus, we override 'contractSize' to 1 for all markets.
+
+Additionally, we need to override 'fetch_position' as it swapped 'notional' with 'contracts'!!! Real ugly. Example below.
+    'id' = None
+    'symbol' = 'BTC/USDC:USDC'
+    'timestamp' = None
+    'datetime' = None
+    'lastUpdateTimestamp' = None
+    'initialMargin' = ???
+    'initialMarginPercentage' = ???
+    'maintenanceMargin' = ???
+    'maintenanceMarginPercentage' = ???
+    'entryPrice' = 85657.0
+    'notional' = 0.0009	<-- This is NOT USD! And this is NOT # Contracts! This is # BTC!
+    'leverage' = 50
+    'unrealizedPnl' = ???
+    'realizedPnl' = ???
+    'contracts' = 77.081445	<-- This is NOT "# contracts"! 0.0009 BTC x markPrice 85646.05
+    'contractSize' = 1.0
+    'marginRatio' = None
+    'liquidationPrice' = None
+    'markPrice' = 85646.05  <-- They use 'markPrice' to calc 'contracts'
+    'lastPrice' = None
+    'collateral' = None
+    'marginMode' = None
+    'side' = 'long'
+    'percentage' = None
+    'hedged' = None
+    'stopLossPrice' = None
+    'takeProfitPrice' = None
 '''
 class Deribit(ccxt.deribit):
     def __init__(self, *args: Dict[str, Any]) -> None:
@@ -26,6 +53,14 @@ class Deribit(ccxt.deribit):
             self.markets[market]['contractSize'] = 1
 
         return self.markets
+    
+    def fetch_position(self, symbol: str, params={}): # type: ignore
+        position = super().fetch_position(symbol=symbol, params=params)
+        pos_usdt = position['contracts']
+        pos_baseccy = position['notional']
+        position['contracts'] = pos_baseccy
+        position['notional'] = pos_usdt
+        return position
 
 class DeribitAsync(ccxtpro.deribit):
     def __init__(self, *args: Dict[str, Any]) -> None:
@@ -38,3 +73,11 @@ class DeribitAsync(ccxtpro.deribit):
             self.markets[market]['contractSize'] = 1
             
         return self.markets
+
+    async def fetch_position(self, symbol: str, params={}): # type: ignore
+        position = await super().fetch_position(symbol=symbol, params=params)
+        pos_usdt = position['contracts']
+        pos_baseccy = position['notional']
+        position['contracts'] = pos_baseccy
+        position['notional'] = pos_usdt
+        return position
