0dd52ff40135e665ae79d163be14c34f2a128731
r0bbar <r0bbarh00d@gmail.com>
2026-01-19 08:09:59 +0800

    strategy_executor.py: log TA only if lo_candles updated

--- siglab_py/algo/strategy_executor.py
+++ siglab_py/algo/strategy_executor.py
@@ -610,6 +610,8 @@ async def main():
         tp_min_percent : float  = param['tp_min_percent']
         executed_position = None
         position_break : bool = False
+        lo_row_timestamp_ms : int = 0
+        lo_candles_updated : bool = True
         while (not position_break):
             try:
                 dt_now = datetime.now()
@@ -882,6 +884,11 @@ async def main():
                         candles_age = dt_now.timestamp() *1000 - lo_row['timestamp_ms']
                         if candles_age < lo_interval_ms:
                             lo_candles_valid = True
+
+                            lo_candles_updated = False
+                            if lo_row['timestamp_ms']!=lo_row_timestamp_ms:
+                                lo_row_timestamp_ms = lo_row['timestamp_ms']
+                                lo_candles_updated = True
 
                             trailing_candles = pd_lo_candles_w_ta \
                                     .tail(param['reversal_num_intervals']) \
@@ -1418,8 +1425,11 @@ async def main():
                     log(f"{tabulate(pd_position_cache.loc[:, 'ob_mid':'ob_best_ask'], headers='keys', tablefmt='psql')}", log_level=LogLevel.INFO)
                     log(f"{tabulate(pd_position_cache.loc[:, 'unreal_live':'max_unreal_open_bps'], headers='keys', tablefmt='psql')}", log_level=LogLevel.INFO)
                     log(f"{tabulate(pd_position_cache.loc[:, 'running_sl_percent_hard':'loss_trailing'], headers='keys', tablefmt='psql')}", log_level=LogLevel.INFO)
-                    log(f"{tabulate(pd_position_cache.loc[:, 'lo_row:datetime':'hi_row_tm1:id'], headers='keys', tablefmt='psql')}", log_level=LogLevel.INFO)
-                    log(f"{tabulate(pd_position_cache.loc[:, strategy_indicators[0]:], headers='keys', tablefmt='psql')}", log_level=LogLevel.INFO)
+
+                    if lo_candles_updated:
+                        log(f"candles_provider updated lo_candles! Latest: {lo_row['datetime']} lo_row_timestamp_ms: {lo_row_timestamp_ms}")
+                        log(f"{tabulate(pd_position_cache.loc[:, 'lo_row:datetime':'hi_row_tm1:id'], headers='keys', tablefmt='psql')}", log_level=LogLevel.INFO)
+                        log(f"{tabulate(pd_position_cache.loc[:, strategy_indicators[0]:], headers='keys', tablefmt='psql')}", log_level=LogLevel.INFO)
 
                     pd_position_cache.to_csv(position_cache_file_name.replace("$GATEWAY_ID$", gateway_id))
                     orderhist_cache.to_csv(orderhist_cache_file_name.replace("$GATEWAY_ID$", gateway_id))
