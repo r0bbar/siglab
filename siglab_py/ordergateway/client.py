c66b3bf6989ae4baeaf6b1abb724197aaab71547
r0bbar <r0bbarh00d@gmail.com>
2026-03-21 06:27:04 +0800

    client.py: bug fix around get_filled_amount - Sometimes exchanges order closed and [fully] filled. But CCTX set 'filled' to Zero (but 'amount' populated properly)

--- siglab_py/ordergateway/client.py
+++ siglab_py/ordergateway/client.py
@@ -281,7 +281,15 @@ class DivisiblePosition(Order):
                                 (
                                     self.executions[order_id]['filled'] 
                                     if 'filled' in self.executions[order_id] and self.executions[order_id]['filled'] 
-                                    else 0
+                                    else ( # This chunk of code is nasty, however necessary. Sometimes exchanges order closed and [fully] filled. But CCTX set 'filled' to Zero here.
+                                        self.executions[order_id]['amount'] 
+                                        if (self.executions[order_id]['amount'] and status.strip().lower()=='closed')
+                                        else (
+                                            self.executions[order_id]['patch']['filled'] # Approximate with slice's dispatched_amount
+                                            if status.strip().lower()=='closed'
+                                            else 0
+                                        ) 
+                                    )
                                 )  * self.multiplier 
                                 for order_id in self.executions 
                                 if self.executions[order_id]['status'].strip().lower() in [ 'closed', 'canceled' ]
