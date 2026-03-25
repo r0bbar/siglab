188845a8e0ce87611204a6e47229adbe8841f3b0
r0bbar <r0bbarh00d@gmail.com>
2026-03-25 16:59:23 +0800

    misc

--- siglab_py/market_data_providers/headlines_provider.py
+++ siglab_py/market_data_providers/headlines_provider.py
@@ -206,7 +206,7 @@ async def main() -> None:
                 try:
                     logger.info(f"new row appended: {pformat(new_headline, indent=2, width=100)}")
                     if loop_counter>0 and (any([ keyword for keyword in param['focus_keywords'] if keyword in new_headline['title'].split(' ') ])): # Don't send flood of slacks in first iteration
-                        dispatch_notification(title=f"[{param['current_filename']}] New headline arrived {new_headline['title'][:15]}", message=new_headline, footer=param['notification']['footer'], params=notification_params, log_level=LogLevel.CRITICAL, logger=logger)
+                        dispatch_notification(title=f"[{param['current_filename']}] New headline arrived {new_headline['title'][:15]} ...", message=new_headline, footer=param['notification']['footer'], params=notification_params, log_level=LogLevel.CRITICAL, logger=logger)
                 except Exception as notification_err:
                     pass # Just swallow
             
