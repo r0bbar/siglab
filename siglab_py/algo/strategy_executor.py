82859fa5d9518f1dfb03de62673e6947b03bff76
r0bbar <r0bbarh00d@gmail.com>
2026-05-27 06:46:59 +0800

    strategy_executor.py: assert(not(allow_entry_final_long and allow_entry_final_short)) ---> Should be AFTER allow_entry_final_long/short UNION with allow_entry_final_long/short

--- siglab_py/algo/strategy_executor.py
+++ siglab_py/algo/strategy_executor.py
@@ -1803,8 +1803,6 @@ async def main():
                                 allow_entry_final_long = allow_entry_func_final_result['long']
                                 allow_entry_final_short = allow_entry_func_final_result['short']
 
-                                assert(not(allow_entry_final_long and allow_entry_final_short))
-
                                 target_price, target_price_long, target_price_short = None, None, None
                                 if allow_entry_final_long and allow_entry_func_final_result['target_price_long']:
                                     target_price_long = round_to_sigfigs(allow_entry_func_final_result['target_price_long'], sigfigs=6)
