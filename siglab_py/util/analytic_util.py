985afb5745afcb868d3ecfc933d05e0ee5b806b5
r0bbar <r0bbarh00d@gmail.com>
2026-06-25 23:41:16 +0800

    revert distance_from_ema_bps bucketing change

--- siglab_py/util/analytic_util.py
+++ siglab_py/util/analytic_util.py
@@ -370,7 +370,7 @@ def compute_candles_stats(
                 str, 
                 Dict[str,Union[float, List[float]]]
             ] = bucket_series(
-                                                    values = [ -2000, -1500, -1000, -750, -500, -400, -300, -200, -150, -100, -75, -50, -25, 0, 25, 50, 100, 150, 200, 300, 400, 500, 750, 1000, 1500, 2000 ], # pd_candles['distance_from_ema_bps'].tolist(),
+                                                    values = pd_candles['distance_from_ema_bps'].tolist(),
                                                     outlier_threshold_percent = 10,
                                                     level_granularity=level_granularity
             )
